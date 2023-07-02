4e7a54365988800946806aa3789363890c3f9112
funzter <cavehive@protonmail.com>
2023-07-02 13:21:21 +0200

    update for mc-1.20.1

--- src/main/java/de/timesnake/channel/bukkit/main/ChannelBukkit.java
+++ src/main/java/de/timesnake/channel/bukkit/main/ChannelBukkit.java
@@ -4,32 +4,35 @@
 
 package de.timesnake.channel.bukkit.main;
 
+import com.moandjiezana.toml.Toml;
 import de.timesnake.channel.core.Channel;
 import de.timesnake.channel.core.SyncRun;
+import de.timesnake.channel.util.ChannelConfig;
 import de.timesnake.channel.util.message.ChannelListenerMessage;
 import de.timesnake.channel.util.message.MessageType;
-import java.time.Duration;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.File;
+import java.time.Duration;
+
 public class ChannelBukkit extends JavaPlugin {
 
-  public static void start(String serverName, Integer proxyPort) {
-    Channel.setInstance(
-        new Channel(Thread.currentThread(), serverName, Bukkit.getPort(), proxyPort) {
-          @Override
-          public void runSync(SyncRun syncRun) {
-            if (getPlugin().isEnabled()) {
-              new BukkitRunnable() {
-                @Override
-                public void run() {
-                  syncRun.run();
-                }
-              }.runTask(getPlugin());
+  public static void start(String serverName) {
+    Channel.setInstance(new Channel(Thread.currentThread(), config, serverName, Bukkit.getPort()) {
+      @Override
+      public void runSync(SyncRun syncRun) {
+        if (getPlugin().isEnabled()) {
+          new BukkitRunnable() {
+            @Override
+            public void run() {
+              syncRun.run();
             }
-          }
-        });
+          }.runTask(getPlugin());
+        }
+      }
+    });
 
     Channel.getInstance().setTimeOut(Duration.ofSeconds(60));
     Channel.getInstance().start();
@@ -52,9 +55,44 @@ public class ChannelBukkit extends JavaPlugin {
   }
 
   private static ChannelBukkit plugin;
+  private static ChannelConfig config;
 
   @Override
   public void onEnable() {
     plugin = this;
+
+    Toml toml = new Toml().read(new File("plugins/channel/config.toml"));
+
+    config = new ChannelConfig() {
+      @Override
+      public String getServerHostName() {
+        return toml.getString("host_name");
+      }
+
+      @Override
+      public String getListenHostName() {
+        return toml.getString("listen_host_name");
+      }
+
+      @Override
+      public String getProxyHostName() {
+        return toml.getString("proxy.host_name");
+      }
+
+      @Override
+      public String getProxyServerName() {
+        return toml.getString("proxy.server_name");
+      }
+
+      @Override
+      public int getPortOffset() {
+        return toml.getLong("port_offset").intValue();
+      }
+
+      @Override
+      public int getProxyPort() {
+        return toml.getLong("proxy.port").intValue();
+      }
+    };
   }
 }
